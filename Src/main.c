aafcf6b2efd58cb36ba8cb7f45dcd2948942cb2c
Roman Piskun <roman.piskun@globallogic.com>
2019-09-23 17:14:03 +0300

    Increase tim6 frequency

--- Src/main.c
+++ Src/main.c
@@ -191,20 +191,10 @@ void DataBufValuesAppend(RAW_DATA_S *pAccelValue, RAW_DATA_S *pGyroValue)
                 pAccelValue,
                 sizeof(RAW_DATA_S));
 
-            // memset(
-            //     &data.payload[idx].bufs[sampleIdx].aValue,
-            //     0xAB,
-            //     sizeof(RAW_DATA_S));
-
             memcpy(
                 &data.payload[idx].bufs[sampleIdx].gValue,
                 pGyroValue,
                 sizeof(RAW_DATA_S));
-
-            // memset(
-            //     &data.payload[idx].bufs[sampleIdx].gValue,
-            //     0xCD,
-            //     sizeof(RAW_DATA_S));
 
             data.payload[idx].readySamplesNum++;
         }
@@ -288,7 +278,7 @@ int main(void)
   bool dataSent = false;
 
   /* USER CODE END 1 */
-
+  
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -364,11 +354,11 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
+  /** Configure the main internal regulator output voltage 
   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -382,7 +372,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -502,10 +492,10 @@ static void MX_USART6_UART_Init(void)
 
 }
 
-/**
+/** 
   * Enable DMA controller clock
   */
-static void MX_DMA_Init(void)
+static void MX_DMA_Init(void) 
 {
 
   /* DMA controller clock enable */
@@ -558,7 +548,7 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
