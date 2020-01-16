27dac486aab60dd9b0e4040fedf16f8310b2f245
Roman Piskun <roman.piskun@globallogic.com>
2020-01-16 11:54:37 +0200

    Fix linker flags in cmake file

--- env.sh
+++
@@ -1,3 +0,0 @@
-export PATH=/opt/st/stm32cubeide_1.0.2/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-arm-embedded.7-2018-q2-update.linux64_1.0.0.201904031358/tools/bin:$PATH
-export CROSS_COMPILE='arm-none-eabi-'
-export ARCH=arm
